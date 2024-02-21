dc512db3977cf5b45a41e21f9da75ea4906b8b40
Henree001 <okaforhenry779@gmail.com>
2024-02-21 13:47:54 +0100

    added the correct shebang

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,4 +1,4 @@
-#!usr/bin/env python3
+#!/usr/bin/env python3
 """ User class for the user object """
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
