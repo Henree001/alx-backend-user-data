b2b26fbbd86fcf72d65063ddddfecac1d536664a
Henree001 <okaforhenry779@gmail.com>
2024-02-21 15:24:55 +0100

    fixed reset_token method in auth class

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -65,9 +65,9 @@ class Auth:
     def get_reset_password_token(self, email: str) -> str:
         """Get a reset password token"""
         try:
-            self._db.find_user_by(email=email)
+            user = self._db.find_user_by(email=email)
             token = _generate_uuid()
-            self._db.update_user(email, reset_token=token)
+            self._db.update_user(user.id, reset_token=token)
             return token
         except Exception:
             raise ValueError
