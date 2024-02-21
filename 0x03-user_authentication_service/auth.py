039fe7484afd7fbe2753642209c24adc387f4383
Henree001 <okaforhenry779@gmail.com>
2024-02-21 15:28:04 +0100

    documented update_password method in auth class

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -73,7 +73,7 @@ class Auth:
             raise ValueError
 
     def update_password(self, reset_token: str, password: str) -> None:
-        """Update a password"""
+        """Updates a user's password"""
         try:
             user = self._db.find_user_by(reset_token=reset_token)
             hashed_password = _hash_password(password)
